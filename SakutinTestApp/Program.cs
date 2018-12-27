377fc57ad8f9d17377f75098b53c033acf654fb1
SergioPerm <itotdel@list.ru>
2018-12-27 22:39:44 +0500

    #5

--- SakutinTestApp/Program.cs
+++ SakutinTestApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading;
 
 namespace Delegates
@@ -7,80 +9,6 @@ namespace Delegates
     internal class Program
     {
 
-        enum typeAttack
-        {
-            Rage = 0,
-            Buzova = 1,
-            Gloom = 2
-        }
-
-        struct Attack
-        {
-
-            public string attackInfo;
-            public int damage;
-            public ConsoleColor colorTalk;
-
-        }
-
-        private static typeAttack GetRandomTypeAttack()
-        {
-            int indexTypeAttack = DateTime.Now.Millisecond % 3;
-            
-            return (typeAttack)indexTypeAttack;
-        }
-        
-        private static Attack CreateAttack(typeAttack typeOfAttack)
-        {
-
-            Attack BossAttack = new Attack();
-            
-            if (typeOfAttack == typeAttack.Rage)
-            {
-                
-                BossAttack.damage = 100;
-                BossAttack.attackInfo = "Босс атаковал с немыслимой яростью своими руками";
-                BossAttack.colorTalk = ConsoleColor.DarkRed;
-                
-            } else if (typeOfAttack == typeAttack.Buzova)
-            {
-                
-                BossAttack.damage = 140;
-                BossAttack.attackInfo = "Босс исполнил новый альбом Ольги бузовой";
-                BossAttack.colorTalk = ConsoleColor.DarkMagenta;
-                
-            } else if (typeOfAttack == typeAttack.Gloom)
-            {
-                
-                BossAttack.damage = 80;
-                BossAttack.attackInfo = "Босс приуныл и рассказал вам о своём долгом пути и дал пару советов, после выпил ритуальный стопарь боярки";
-                BossAttack.colorTalk = ConsoleColor.DarkGray;
-                
-            }
-            else
-            {
-                BossAttack.damage = 0;
-                BossAttack.attackInfo = "Неизвестная атака";
-                BossAttack.colorTalk = ConsoleColor.Red;
-            }
-
-            return BossAttack;
-
-        }
-        
-        private static void BossAttack(Attack BossAttack, ref int Health, ref int Armor)
-        {
-
-            ConsoleColor oldColor = Console.ForegroundColor;
-            
-            Console.ForegroundColor = BossAttack.colorTalk;
-            Console.WriteLine(BossAttack.attackInfo);
-            
-            Console.ForegroundColor = oldColor;
-
-            Health -= (BossAttack.damage - Armor);
-        }
-
         private static void InformPlayer(string info, ConsoleColor infoColor)
         {
             ConsoleColor oldColor = Console.ForegroundColor;
@@ -88,10 +16,16 @@ namespace Delegates
             Console.WriteLine(info);
             Console.ForegroundColor = oldColor;
         }
+
+        public static BossAttack[] Attacks =
+        {
+            new BossAttack("Босс атаковал с немыслимой яростью своими руками", ConsoleColor.DarkRed, 100),
+            new BossAttack("Босс исполнил новый альбом Ольги бузовой", ConsoleColor.DarkMagenta, 140),
+            new BossAttack("Босс приуныл и рассказал вам о своём долгом пути и дал пару советов, после выпил ритуальный стопарь боярки", ConsoleColor.DarkGray, 80)
+        }; 
         
         public static void Main(string[] args)
-        {
-           
+        {  
             InformPlayer("Босс может атаковать в двух режимах: все атаки по очереди и случайной атакой", ConsoleColor.Yellow);
             
             int Health = 1000;
@@ -109,10 +43,13 @@ namespace Delegates
                 Console.Clear();
   
                 InformPlayer("У вас здоровья: " + Health, ConsoleColor.Red);
-   
-                Attack attackData = CreateAttack(isRandomAttack ? GetRandomTypeAttack() : (typeAttack) attackNumber);
-                BossAttack(attackData, ref Health, ref Armor);
-                                
+
+                int AttackIndex = isRandomAttack ? DateTime.Now.Millisecond % 3 : attackNumber;
+                
+                BossAttack CurrentAttack = Attacks[AttackIndex];
+                CurrentAttack.ApplyDamage(Armor, ref Health);
+                CurrentAttack.ShowInfo();
+                                                
                 attackNumber++;
                 attackNumber = attackNumber > 2 ? 0 : attackNumber;
                 
@@ -122,4 +59,34 @@ namespace Delegates
             InformPlayer("Бой закончен, вы погибли", ConsoleColor.DarkGray);
         }
     }
+
+    class BossAttack
+    {
+        
+        private string _damageInfo;
+        private ConsoleColor _damageColor;
+        private int _damage; 
+
+        public BossAttack(string DamageInfo, ConsoleColor DamageColor, int Damage)
+        {
+            this._damageInfo = DamageInfo;
+            this._damageColor = DamageColor;
+            this._damage = Damage;
+        }
+
+        public void ApplyDamage(int Armor, ref int Health)
+        {
+            Health -= this._damage - Armor;
+        }
+
+        public void ShowInfo()
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+            
+            Console.ForegroundColor = this._damageColor;
+            Console.WriteLine(this._damageInfo);
+            
+            Console.ForegroundColor = oldColor;
+        }
+    }  
 }
